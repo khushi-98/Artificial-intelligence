d8fcc1998f862056787b09fcaf178b75048b827f
Khushi Bansal <102850725+khushi-98@users.noreply.github.com>
2024-04-28 11:33:09 +0530

    Add files via upload

---
+++ week4_ques1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=" ")
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Example usage
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+print("Breadth-First Search:")
+bfs(graph, 'A')
+
+
+# In[ ]:
+
+
+
+
